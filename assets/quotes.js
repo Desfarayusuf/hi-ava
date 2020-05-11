1a948ec0c3fdf8616a76c8371d27052abe83158f
Dsvrys <60470765+Desfarayusuf@users.noreply.github.com>
2020-05-11 14:48:57 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "Me", quotes: "Whatever happens, keep breathing......" },
+    { author: "Me", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "Me", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "Me", quotes: "I love you." },
+    { author: "Me", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "Me", quotes: "I love you." },
+    { author: "Me", quotes: "I'm sorry for loving you." },
 ];
